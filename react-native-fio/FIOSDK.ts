da36316871bac62df721ad4fa801413f7ce573eb
shawn arney <shawn@arneyconsulting.com>
2019-04-23 11:20:54 -0600

    todo’s

--- react-native-fio/FIOSDK.ts
+++ react-native-fio/FIOSDK.ts
@@ -3,6 +3,17 @@ import * as queries from './transactions/queries'
 import { Transactions } from './transactions/Transactions';
 import { RecordSendRequest }  from './entities/RecordSendRequest';
 
+// todo's:
+// How do we do unit tests?  We should have unit tests.
+
+// Add a new method called: getFioPublicAddress()  this will return the fio public key (which is currently the generated actor value)
+
+// change 'pendingFioRequests' to 'getpendingFioRequests'
+// change 'sentFioRequests' to 'getSentFioRequests'
+
+
+// the difference between fioAddress i.e. alice.brd AND fioPublicAddress i.e. '0x132432'
+
 export class FIOSDK{
     transactions:Transactions 
     constructor(baseUrl:string,publicKey:string,privateKey:string){
@@ -27,6 +38,7 @@ export class FIOSDK{
         return recordSend.execute();
     }
 
+// spell out the parameter name i.e. fioRequestId
     rejectFundsRequest(fioreqid: string):Promise<any>{
         let rejectFundsRequest = new SignedTransactions.RejectFundsRequest(fioreqid)
         return rejectFundsRequest.execute();
@@ -42,22 +54,26 @@ export class FIOSDK{
         return availabilityCheck.execute();
     }
     
+// parameter name should be: fioPublicAddress
     getFioBalance(fioAddress: string):Promise<any>{
         let getFioBalance = new queries.GetFioBalance(fioAddress);
         return getFioBalance.execute();
     }
 
+// parameter name should be: fioPublicAddress
     getNames(fioAddress:string):Promise<any>{
         let getNames = new queries.GetNames(fioAddress);
         return getNames.execute()
 
     }
 
+// parameter name should be: fioPublicAddress
     pendingFioRequests(publicAddress:string):Promise<any>{
         let pendingFioRequests = new queries.PendingFioRequests(publicAddress);
         return pendingFioRequests.execute()
     }
 
+// parameter name should be: fioPublicAddress
     sentFioRequests(fioAddress:string):Promise<any>{
         let sentFioRequest = new queries.SentFioRequests(fioAddress);
         return sentFioRequest.execute()
