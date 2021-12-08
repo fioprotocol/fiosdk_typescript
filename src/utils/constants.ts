13a9096af62b06ea9b88c468cfd0e02b7be409d4
andreyvEze <andreyv@eze.tech>
2021-12-08 18:44:44 +0200

    Add stakeFioTokens method

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -9,6 +9,7 @@ export class Constants {
     RequestNewFunds: 'new_funds_request',
     TransferTokensKey: 'transfer_tokens_pub_key',
     TransferTokensFioAddress: 'transfer_tokens_fio_address',
+    StakeFioTokens: 'stake_fio_tokens',
   }
 
   public static feeNoAddressOperation: string[] = [
