5ddb7fa573adae90d1212b00aac9aeca4e47a80b
Vadym Karpenko <vadim.karpenko.306@gmail.com>
2023-01-24 15:07:16 +0200

    Multicast servers. Changed fiosdk use array of api urls.

---
+++ src/utils/utils.ts
@@ -0,0 +1,67 @@
+const PROMISE_TIMEOUT = 5000;
+
+const snooze: Function = (ms: number) =>
+  new Promise((resolve: Function) => setTimeout(resolve, ms))
+
+export function shuffleArray(array: Array<any>) {
+  let currentIndex = array.length
+  let temporaryValue
+  let randomIndex
+
+  // While there remain elements to shuffle...
+  while (currentIndex !== 0) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex -= 1
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex]
+    array[currentIndex] = array[randomIndex]
+    array[randomIndex] = temporaryValue
+  }
+
+  return array
+}
+
+export async function asyncWaterfall(
+  asyncFuncs: Array<Function>,
+  timeoutMs: number = PROMISE_TIMEOUT,
+): Promise<any> {
+  let pending = asyncFuncs.length
+  const promises: Array<Promise<any>> = []
+  for (const func of asyncFuncs) {
+    const index = promises.length
+    promises.push(
+      func().catch((e: Error & { index: number }) => {
+        e.index = index
+        throw e
+      }),
+    )
+    if (pending > 1) {
+      promises.push(
+        new Promise(resolve => {
+          snooze(timeoutMs).then(() => {
+            resolve('async_waterfall_timed_out')
+          })
+        })
+      )
+    }
+    try {
+      const result = await Promise.race(promises)
+      if (result === 'async_waterfall_timed_out') {
+        promises.pop()
+        --pending
+      } else {
+        return result
+      }
+    } catch (error: any) {
+      const i = error.index
+      promises.splice(i, 1)
+      promises.pop()
+      --pending
+      if (!pending) {
+        throw error
+      }
+    }
+  }
+}
